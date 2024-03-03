316f8f25d95a5096630e43c96374143b8e916a55
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-03-04 04:24:46 +0530

    Form bug fix

--- src/Redux/formSlice.js
+++ src/Redux/formSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const formSlice = createSlice({
     name:"form",
     initialState:{
-        status:"false",
+        status:false,
         data:[]
     },
     reducers:{
