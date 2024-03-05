432ef6bb92293ee9f8c26bf784e081bf5b4bb81d
sammyy99 <samarthbhardwaj1999@gmail.com>
2024-03-05 19:50:50 +0530

    Download cv added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ import { Typewriter } from "react-simple-typewriter";
 import { homeData } from "../Utils&Constants/constants";
 import { useDispatch } from "react-redux";
 import { getform } from "../Redux/formSlice";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -12,12 +13,19 @@ const Home = () => {
   };
 
   return (
-    <div id="home" className=" relative font-mono font-semibold w-full h-full pb-7 md:pb-20">
-      
-      <video autoPlay loop muted className="absolute h-full w-full object-cover">
+    <div
+      id="home"
+      className=" relative font-mono font-semibold w-full h-full pb-7 md:pb-20"
+    >
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute h-full w-full object-cover"
+      >
         <source src="bgAbout.mp4" type="video/mp4" />
       </video>
-      
+
       <div className="md:h-40 h-24"></div>
 
       <div className="text-white md:flex md:justify-between mx-2 md:mx-6 py-1 md:py-0 bg-black bg-opacity-80 rounded-lg z-10 relative">
@@ -57,6 +65,21 @@ const Home = () => {
               className="bg-black border border-green-500 px-4 py-1 rounded-lg hover:cursor-pointer hover:text-green-300 hover:border-green-300 hover:text-xl md:hover:text-3xl transition-all translate-300"
             >
               Get in touch
+            </button>
+
+            <button className="bg-black border mx-2 md:mx-3  border-green-500 px-4 py-1 rounded-lg hover:cursor-pointer hover:text-green-300 hover:border-green-300 hover:text-xl md:hover:text-3xl transition-all translate-300">
+              <a
+                href="/Resume.pdf" // Replace "/path/to/your/cv.pdf" with the actual path to your CV file
+                download="Resume.pdf" // This will be the name of the downloaded file
+                className="flex items-center"
+              >
+                <div className="flex">
+                  <div>Download CV</div>
+                  <div className="mx-2 -mt-[2px]">
+                    <FileDownloadIcon />
+                  </div>
+                </div>
+              </a>
             </button>
           </div>
         </div>
